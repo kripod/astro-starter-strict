f42ec27fe41271d5b78aef656e478ba4f62b5e6c
Kristóf Poduszló <kripod@protonmail.com>
2023-09-16 08:28:14 +0200

    fix: enforce using trailing slash in URLs

--- astro.config.js
+++ astro.config.js
@@ -3,5 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  trailingSlash: "always",
   integrations: [mdx(), tailwind()],
 });
